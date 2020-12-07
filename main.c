9d842523157bd3bab682c8096e75413d5e9b5a51
- <->
2020-12-07 10:38:05 +0900

    14_4

--- main.c
+++ main.c
@@ -1,34 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "linkedList.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-struct Book{
-	int number;
-	char title[20];
-};
-
 int main(int argc, char *argv[]) {
-	int i;
-	struct Book **bookself;
+	int n[5];
 	
-	bookself= (struct Book**)malloc(3*sizeof(struct Book*)); //3 pointer allocations
-	for(i=0;i<3;i++)
-		bookself[i]= (struct Book*)malloc(3*sizeof(struct Book));//10 allocation
+	printf("input 5 inegers : ");
+	scanf("%d %d %d %d %d", n,n+1,n+2,n+3,n+4);
 	
-	bookself[1][3].number =5;
-	strcpy(bookself[1][3].title, "C++ Programming");
+	insertDataToTail(n[0]);
+	insertDataToTail(n[1]);
+	insertDataToTail(n[2]);
+	insertDataToTail(n[3]);
+	insertDataToTail(n[4]);
 	
-	(bookself[2]+4)->number =3;
-	strcpy((bookself[2]+4)->title, "Communications Theory");
+	print_list();
 	
-	printf("book(1,3): %i, %s\n",(bookself[1]+3)->number, (bookself[1]+3)->title);
-	printf("book(2,4): %i, %s\n", bookself[2][4].number , bookself[2][4].title);
-	
-	//free allocatios
-	for(i=0;i<3;i++)
-		free(bookself[i]);
-	free(bookself);
-	
-	return 0;
+	print_node(2);
+	 
+//	return 0;
 }
