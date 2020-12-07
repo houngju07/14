b6ed81a1f829e949b2eb2de439565782872ce0bc
- <->
2020-12-07 09:42:07 +0900

    14_3

--- main.c
+++ main.c
@@ -9,23 +9,26 @@ struct Book{
 };
 
 int main(int argc, char *argv[]) {
-	struct Book *p;
+	int i;
+	struct Book **bookself;
 	
-	p=(struct Book*)malloc(2*sizeof(struct Book)); //dynamic allocation of 2 Book structure instances
+	bookself= (struct Book**)malloc(3*sizeof(struct Book*)); //3 pointer allocations
+	for(i=0;i<3;i++)
+		bookself[i]= (struct Book*)malloc(3*sizeof(struct Book));//10 allocation
 	
-	if(p == NULL){
-		printf("memory allocation error!\n");
-		return -1;
-	}
+	bookself[1][3].number =5;
+	strcpy(bookself[1][3].title, "C++ Programming");
 	
-	p->number =1;
-	strcpy(p->title, "C programming");
+	(bookself[2]+4)->number =3;
+	strcpy((bookself[2]+4)->title, "Communications Theory");
 	
-	(p+1)->number=2;
-	strcpy((p+1)->title, "Electronics");
+	printf("book(1,3): %i, %s\n",(bookself[1]+3)->number, (bookself[1]+3)->title);
+	printf("book(2,4): %i, %s\n", bookself[2][4].number , bookself[2][4].title);
 	
-	printf("%s %s\n", p->title,(p+1)->title);
-
-	free(p);
+	//free allocatios
+	for(i=0;i<3;i++)
+		free(bookself[i]);
+	free(bookself);
+	
 	return 0;
 }
